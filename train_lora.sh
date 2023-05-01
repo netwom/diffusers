93407c85bb3ed429e1f5dd493052a0d0bc65ac46
Dmitriy Kutuzov <dmitry375@gmail.com>
2023-05-01 21:11:03 +0200

    params updated

--- train_lora.sh
+++ train_lora.sh
@@ -4,7 +4,7 @@ accelerate launch examples/dreambooth/train_dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="$INSTANCE_PROMPT" \
+  --instance_prompt=$INSTANCE_PROMPT \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
@@ -13,6 +13,6 @@ accelerate launch examples/dreambooth/train_dreambooth_lora.py \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=$MAX_TRAIN_STEPS \
-  --validation_prompt="$VALIDATION_PROMPT" \
+  --validation_prompt=$VALIDATION_PROMPT \
   --validation_epochs=50 \
   --seed="0"
